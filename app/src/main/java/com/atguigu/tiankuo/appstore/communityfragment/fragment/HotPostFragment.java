31fbe612288a84e5848101d89f3ac109edc91fb3
tiankuo-android <1226147264@qq.com>
2017-06-17 16:11:46 +0800

    发现-热帖--置顶，精华，热门

--- app/src/main/java/com/atguigu/tiankuo/appstore/communityfragment/fragment/HotPostFragment.java
+++ app/src/main/java/com/atguigu/tiankuo/appstore/communityfragment/fragment/HotPostFragment.java
@@ -1,10 +1,23 @@
 package com.atguigu.tiankuo.appstore.communityfragment.fragment;
 
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.tiankuo.appstore.R;
 import com.atguigu.tiankuo.appstore.base.BaseFragment;
+import com.atguigu.tiankuo.appstore.communityfragment.adapter.HotPostListViewAdapter;
+import com.atguigu.tiankuo.appstore.domain.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import okhttp3.Call;
 
 /**
  * 作者：田阔
@@ -12,17 +25,67 @@ import com.atguigu.tiankuo.appstore.base.BaseFragment;
  * Created by Administrator on 2017/6/16 0016.
  */
 public class HotPostFragment extends BaseFragment {
-    private TextView textView;
+
+    @InjectView(R.id.lv_hot_post)
+    ListView lvHotPost;
+    private List<HotPostBean.ResultBean> result;
+
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(23);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_hot_post, null);
+        ButterKnife.inject(this, view);
+        return view;
     }
 
     @Override
     public void initData() {
-        textView.setText("热帖内容");
+        super.initData();
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        OkHttpUtils
+                .get()
+                .url(Constants.HOT_POST_URL)
+                .id(100)
+                .build()
+                .execute(new MyStringCallback());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
+    }
+
+    private class MyStringCallback extends StringCallback {
+        @Override
+        public void onError(Call call, Exception e, int id) {
+            Log.e("TAG", "请求失败==" + e.getMessage());
+        }
+
+        @Override
+        public void onResponse(String response, int id) {
+            Log.e("TAG", "请求成功==");
+            switch (id) {
+                case 100:
+                    if (response != null) {
+                        processData(response);
+                        HotPostListViewAdapter adapter = new HotPostListViewAdapter(mContext, result);
+                        lvHotPost.setAdapter(adapter);
+                    }
+                    break;
+                case 101:
+                    Toast.makeText(mContext, "https", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+
+    }
+
+    private void processData(String json) {
+        HotPostBean hotPostBean = JSONObject.parseObject(json, HotPostBean.class);
+        result = hotPostBean.getResult();
+
     }
 }
